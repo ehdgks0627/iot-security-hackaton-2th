87fb4c5b59669c65055d0d8b0f72efff06e64abb
ehdgks0627 <ehdgks0627@naver.com>
2017-10-29 00:42:08 +0900

    implement check

--- deeps/views.py
+++ deeps/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-#from keras.models import load_model
 import json
+import requests
 
 #model = load_model("model.h5")
 
 @csrf_exempt
 def check(request):
-    x_data = json.loads(request.REQUEST.get("features"))
-    #model.predict(x_data)
-    #train here a
-    return HttpResponse("test~~")
+    req = json.loads(request.body.decode("utf-8"))
+    PK = req["PK"]
+    time = req["time"]
+    features = req["features"]
+    #request.POST("",data={})
+    isInfacted = True
+    return HttpResponse(str({"check": isInfacted, "time": time}).replace("'", '"'))
