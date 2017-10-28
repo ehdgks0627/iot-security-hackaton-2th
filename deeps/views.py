33f8bf786772bd94e3b5007cc2b97138858a9c71
ehdgks0627 <ehdgks0627@naver.com>
2017-10-28 12:39:26 +0900

    add test function

--- deeps/views.py
+++ deeps/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from keras.models import load_model
 # Create your views here.
+
+#model = load_model("model.h5")
+
+def check(request):
+    return HttpResponse("test~~")
